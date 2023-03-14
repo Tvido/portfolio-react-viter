e64176ce1c569b5666fb963cd5ab148681cf6527
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-14 15:06:55 +0200

    created basic imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-function App() {
+import React from "react";
+import { About, Contacts, Footer, Header, Projects, Skills } from "./views";
+
+const App = () => {
   return <h1>Hello</h1>;
-}
+};
 
 export default App;
---
+++ src/views/Contacts/Contacts.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Contacts.scss";
+
+const Contacts = () => {
+  return <div></div>;
+};
+
+export default Contacts;
