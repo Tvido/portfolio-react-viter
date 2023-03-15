3affe932614e62393e95bca1cc543ed4d797d47f
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-15 21:26:49 +0200

    add additional styles for navbar

--- src/helpers/images.jsx
+++ src/helpers/images.jsx
@@ -1,3 +1,5 @@
 import logo from "../assets/images/logo.svg";
+import circle from "../assets/images/circle.svg";
+import bgWhite from "../assets/images/bgWhite.png";
 
-export default { logo };
+export default { logo, circle, bgWhite };
