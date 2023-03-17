b0ba02a75db1947750572b5b3a090b2fe7b71879
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-17 23:05:25 +0200

    added footer

--- src/views/Footer/Footer.jsx
+++ src/views/Footer/Footer.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 import "./Footer.scss";
 
-import AppWrapper from "../../components/AppWrapper/AppWrapper";
-
 const Footer = () => {
   return (
-    <div>
-      <h2>Footer Section</h2>
+    <div className="copyright">
+      <div className="p-text">@2023 Tvido</div>
     </div>
   );
 };
