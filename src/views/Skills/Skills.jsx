85df8ea7a3b985b40f926b5c1eab604e7a9d129e
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-17 23:09:43 +0200

    started skills section

--- src/views/Skills/Skills.jsx
+++ src/views/Skills/Skills.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 import "./Skills.scss";
+
+import { skills } from "../../assets/data";
 
 import AppWrapper from "../../components/AppWrapper/AppWrapper";
 
 const Skills = () => {
   return (
-    <div>
-      <h2>Skills Section</h2>
-    </div>
+    <>
+      <h2 className="head-text">
+        My <span>Skills</span>
+      </h2>
+
+      <div className="skills-container">
+        <motion.div className="skills-list">
+          {skills.map((item) => (
+            <motion.div
+              key={item.id}
+              whileInView={{ scale: [0, 1] }}
+              transition={{
+                duration: 0.25,
+              }}
+              className="skills-item app__flex"
+            >
+              <div className="app__flex">
+                <img src={item.icon} alt={item.title} />
+              </div>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </>
   );
 };
 
