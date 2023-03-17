cdca76cedaed417cd112b9c4dc99253f9767d719
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-17 23:06:12 +0200

    Update index.js

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,3 +1,4 @@
 export { default as filter } from "./filterData";
 export { default as navbar } from "./navbarData";
 export { default as portfolios } from "./portfolioData";
+export { default as skills } from "./skillsData";
