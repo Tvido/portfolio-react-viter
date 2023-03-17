892e18dcdbf8260cd687eb4d8c56d025c004e292
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-17 23:05:09 +0200

    Update AppWrapper.jsx

--- src/components/AppWrapper/AppWrapper.jsx
+++ src/components/AppWrapper/AppWrapper.jsx
@@ -8,15 +8,11 @@ import SocialMedia from "../SocialMedia/SocialMedia";
 const AppWrapper = (Component, idName, classNames) =>
   function Wrapper() {
     return (
-      <div id={idName} className={`app__container ${classNames}`}>
+      <div id={idName} className={`app__container wrapper ${classNames}`}>
         <SocialMedia />
 
         <div className="app__wrapper app__flex">
           <Component />
-
-          <div className="copyright">
-            <div className="p-text">@2023 Tvido</div>
-          </div>
         </div>
 
         <NavigationDots active={idName} />
