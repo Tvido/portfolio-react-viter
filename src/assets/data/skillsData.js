5c476dcf6a34684ab4f8eefe791a66aabdac85d8
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-17 23:06:01 +0200

    added data for Skills section

---
+++ src/assets/data/skillsData.js
@@ -0,0 +1,88 @@
+const skillsData = [
+  {
+    id: 1,
+    title: "Angular",
+    bgColor: "",
+    icon: "https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg",
+  },
+  {
+    id: 2,
+    title: "CSS3",
+    bgColor: "",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/1200px-CSS.3.svg.png",
+  },
+  {
+    id: 3,
+    title: "Figma",
+    bgColor: "",
+    icon: "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+  },
+  {
+    id: 4,
+    title: "GIT",
+    bgColor: "",
+    icon: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+  },
+  {
+    id: 5,
+    title: "HTML5",
+    bgColor: "",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425",
+  },
+  {
+    id: 6,
+    title: "Java Script",
+    bgColor: "",
+    icon: "https://www.freepnglogos.com/uploads/javascript-png/javascript-vector-logo-yellow-png-transparent-javascript-vector-12.png",
+  },
+  {
+    id: 7,
+    title: "Redux",
+    bgColor: "",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png",
+  },
+  {
+    id: 8,
+    title: "SCSS",
+    bgColor: "",
+    icon: "https://cdn-icons-png.flaticon.com/512/5968/5968358.png",
+  },
+  {
+    id: 9,
+    title: "React.JS",
+    bgColor: "",
+    icon: "https://fernandosg.github.io/img/reactjsicon.png",
+  },
+  {
+    id: 10,
+    title: "node.js",
+    bgColor: "",
+    icon: "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png",
+  },
+  {
+    id: 11,
+    title: "TypeScript",
+    bgColor: "",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1024px-Typescript_logo_2020.svg.png",
+  },
+  {
+    id: 12,
+    title: "MongoDB",
+    bgColor: "",
+    icon: "https://www.pngall.com/wp-content/uploads/13/Mongodb-Transparent.png",
+  },
+  {
+    id: 13,
+    title: "React Icons",
+    bgColor: "",
+    icon: "https://camo.githubusercontent.com/48d099290b4cb2d7937bcd96e8497cf1845b54a810a6432c70cf944b60b40c77/68747470733a2f2f7261776769742e636f6d2f676f72616e67616a69632f72656163742d69636f6e732f6d61737465722f72656163742d69636f6e732e737667",
+  },
+  {
+    id: 14,
+    title: "Axios",
+    bgColor: "",
+    icon: "https://branditechture.agency/brand-logos/wp-content/uploads/wpdm-cache/Axios-900x0.png",
+  },
+];
+
+export default skillsData;
