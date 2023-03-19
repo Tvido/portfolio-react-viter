e8844848ff30bc3360c5e30b2d41590d5e01df94
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-19 19:54:56 +0200

    added main section

--- src/helpers/images.jsx
+++ src/helpers/images.jsx
@@ -1,5 +1,6 @@
 import logo from "../assets/images/logo.svg";
 import circle from "../assets/images/circle.svg";
 import bgWhite from "../assets/images/bgWhite.png";
+import hero from "../assets/images/photo.jpg";
 
-export default { logo, circle, bgWhite };
+export default { logo, circle, bgWhite, hero };
--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -1,14 +1,46 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import "./Header.scss";
 
 import AppWrapper from "../../components/AppWrapper/AppWrapper";
 import MotionWrapper from "../../components/MotionWrapper/MotionWrapper";
+import images from "../../helpers/images";
 
 const Header = () => {
   return (
-    <div>
-      <h2>Header Section</h2>
+    <div className="">
+      <h3 className="head-text">
+        Hello World, I am <span>Tvido</span>
+      </h3>
+
+      <div className="header__container">
+        <motion.div
+          className="header__data-info"
+          whileInView={{ scale: [0, 1] }}
+          transition={{
+            duration: 0.5,
+          }}
+        >
+          <h3>Lorem, ipsum dolor.</h3>
+          <p className="p-text">
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Quos modi cum, est possimus optio non et ratione
+            fugit magnam aut incidunt nemo maiores ducimus quam neque minus
+            expedita maxime iure.
+          </p>
+        </motion.div>
+
+        <motion.div
+          className="header__data-photo"
+          whileInView={{ scale: [0, 1] }}
+          transition={{
+            duration: 0.5,
+          }}
+        >
+          <img src={images.hero} alt="Tvido" />
+        </motion.div>
+      </div>
     </div>
   );
 };
