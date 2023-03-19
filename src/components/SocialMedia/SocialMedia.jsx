085eddc30fa64a38cfde31e37beec5d7aa50b165
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-19 20:52:10 +0200

    updated social links

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -8,16 +8,34 @@ const SocialMedia = () => {
   return (
     <div className="app__social-list">
       <div>
-        <ImLinkedin />
+        <a
+          href="https://www.linkedin.com/in/viterd"
+          target="_blank"
+          rel="noopener noreferer"
+        >
+          <ImLinkedin />
+        </a>
       </div>
 
       <div>
-        <ImGithub />
+        <a
+          href="https://github.com/Tvido"
+          target="_blank"
+          rel="noopener noreferer"
+        >
+          <ImGithub />
+        </a>
       </div>
 
-      <div>
-        <GrInstagram />
-      </div>
+      {/* <div>
+        <a
+          href="https://www.linkedin.com/in/viterd"
+          target="_blank"
+          rel="noopener noreferer"
+        >
+          <GrInstagram />
+        </a>
+      </div> */}
     </div>
   );
 };
