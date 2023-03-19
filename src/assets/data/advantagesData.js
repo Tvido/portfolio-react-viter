daec73968d1448a265dd7057e17bfbeaa3127417
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-19 13:16:46 +0200

    added mock data for about section

---
+++ src/assets/data/advantagesData.js
@@ -0,0 +1,24 @@
+const advantagesData = [
+  {
+    id: 1,
+    title: "responsibility",
+    description: "Lorem ipsum Lorem ipsum ",
+    picture: "https://cdn-icons-png.flaticon.com/512/2726/2726565.png",
+  },
+  {
+    id: 2,
+    title: "technology stack",
+    description:
+      "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ",
+    picture:
+      "https://www.varianceinfotech.com/assets/front/images/banner-images/mern-stack-banner-image.png",
+  },
+  {
+    id: 3,
+    title: "versatility",
+    description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ",
+    picture: "https://cdn-icons-png.flaticon.com/512/6961/6961811.png",
+  },
+];
+
+export default advantagesData;
--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -2,3 +2,4 @@ export { default as filter } from "./filterData";
 export { default as navbar } from "./navbarData";
 export { default as portfolios } from "./portfolioData";
 export { default as skills } from "./skillsData";
+export { default as advantages } from "./advantagesData";
