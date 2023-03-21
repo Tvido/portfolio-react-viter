06b655c35a2d3c895516dd43454d0716256f807a
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-21 19:26:30 +0200

    Update portfolioData.js

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -1,39 +1,39 @@
 const portfolioData = [
   {
     id: 1,
-    title: "Online Shop Site",
+    title: "Liceum 20",
     description: "Lorem ipsum dolor",
-    codeLink: "#",
-    demoLink: "#",
+    codeLink: "https://github.com/Tvido/liceum20",
+    demoLink: "https://liceum20.com.ua/",
     picture:
       "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1",
-    type: ["Angular", "All"],
+    type: ["React.js", "All"],
   },
   {
     id: 2,
-    title: "Photo-Portfolio Site",
+    title: "Photographer Portfolio Site",
     description: "Lorem ipsum dolor",
-    codeLink: "#",
-    demoLink: "#",
+    codeLink: "https://github.com/Tvido/boyko",
+    demoLink: "https://tvido.github.io/boyko/",
     picture:
       "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1",
     type: ["HTML/CSS/JS", "All"],
   },
   {
     id: 3,
-    title: "Liceum #20 Site",
+    title: "Green City | Collaboration",
     description: "Lorem ipsum dolor",
-    codeLink: "#",
-    demoLink: "#",
+    codeLink: "https://github.com/ita-social-projects/GreenCityClient/",
+    demoLink: "https://www.testgreencity.ga/#/greenCity",
     picture:
       "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1",
-    type: ["React.JS", "All"],
+    type: ["Angular", "All"],
   },
   {
     id: 4,
-    title: "Job Seeker",
+    title: "Job Seeker Sike",
     description: "Lorem ipsum dolor",
-    codeLink: "#",
+    codeLink: "https://github.com/Tvido/refugees",
     demoLink: "#",
     picture:
       "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1",
@@ -43,8 +43,29 @@ const portfolioData = [
     id: 5,
     title: "Photoschool Site",
     description: "Lorem ipsum dolor",
-    codeLink: "#",
-    demoLink: "#",
+    codeLink: "https://github.com/Tvido/photoschool",
+    demoLink: "https://tvido.github.io/photoschool/",
+    picture:
+      "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1",
+    type: ["HTML/CSS/JS", "All"],
+  },
+  {
+    id: 6,
+    title: "Flight Data info | Collaboration",
+    description: "Lorem ipsum dolor",
+    codeLink:
+      "https://github.com/if137angular/if137angular/commits?author=Tvido",
+    demoLink: "https://if137angular.github.io/if137angular/#/",
+    picture:
+      "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1",
+    type: ["Angular", "All"],
+  },
+  {
+    id: 7,
+    title: "Homemade Ice-Cream Site | Collaboration",
+    description: "Lorem ipsum dolor",
+    codeLink: "https://github.com/Boombling/IceCream_9/commits?author=Tvido",
+    demoLink: "https://github.com/Boombling/IceCream_9",
     picture:
       "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1",
     type: ["HTML/CSS/JS", "All"],
