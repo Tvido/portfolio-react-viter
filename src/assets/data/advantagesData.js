0d0408596e9faa4a10f50b804cb1d42b48f27347
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-22 19:07:43 +0200

    Update advantagesData.js

--- src/assets/data/advantagesData.js
+++ src/assets/data/advantagesData.js
@@ -2,21 +2,22 @@ const advantagesData = [
   {
     id: 1,
     title: "responsibility",
-    description: "Lorem ipsum Lorem ipsum ",
+    description: "Conscious attitude to requirements, duties and tasks",
     picture: "https://cdn-icons-png.flaticon.com/512/2726/2726565.png",
   },
   {
     id: 2,
     title: "technology stack",
     description:
-      "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ",
+      "The success of any IT product directly depends on how well and competently the stack for its creation are selected",
     picture:
       "https://www.varianceinfotech.com/assets/front/images/banner-images/mern-stack-banner-image.png",
   },
   {
     id: 3,
     title: "versatility",
-    description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ",
+    description:
+      "The most modern and diverse stack used to implement any goals",
     picture: "https://cdn-icons-png.flaticon.com/512/6961/6961811.png",
   },
 ];
