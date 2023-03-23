da5e5cfcd05bd2878b5d94d09f535cb4ae5d41c3
Volodymyr Viter <mad.tvido@gmail.com>
2023-03-23 22:35:57 +0200

    updated hero section info

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -24,18 +24,18 @@ const Header = () => {
         >
           <h3>Web Developer</h3>
           <p className="p-text">
-            I am a Junior front-end developer with more than two years of
-            experience. I really like to code and create something awesome,
-            something that the user can then enjoy. I completed my studies at
-            the GoIT school and SoftServe internship, I learned HTML, CSS,
-            Javascript, React.js, Git, Github, Angular, Node.js. I have
-            experience in developing personal portfolio sites for the most
-            demanding clients; and voluminous SPA educational sites for those
-            with a thirst for learning. Also, I know the basics of UI/UX Design,
-            which allows me to develop not only functional, modern and effective
-            sites, but also beautiful ones. I can work both independently and in
-            a team. I know how to communicate with clients and fulfill assigned
-            tasks and get pleasure from work.
+            I am a Junior front-end developer with 2+ years of experience. I
+            really like to code and create something awesome, something that the
+            user can then enjoy. I completed my studies at the GoIT school and
+            SoftServe internship, I learned HTML, CSS, Javascript, React.js,
+            Git, Github, Angular, Node.js. I have experience in developing
+            personal portfolio sites for the most demanding clients; and
+            voluminous SPA educational sites for those with a thirst for
+            learning. Also, I know the basics of UI/UX Design, which allows me
+            to develop not only functional, modern and effective sites, but also
+            beautiful ones. I can work both independently and in a team. I know
+            how to communicate with clients and fulfill assigned tasks and get
+            pleasure from work.
           </p>
         </motion.div>
 
